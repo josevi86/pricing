ddd364f9fbd39073e8b47c82b6b4e528ca5a90f1
Jose Vicente <j.larasolana@gmail.com>
2022-01-30 16:48:43 +0100

    Unused imports removed

--- src/main/java/com/zara/pricing/controller/PriceController.java
+++ src/main/java/com/zara/pricing/controller/PriceController.java
@@ -1,7 +1,6 @@
 package com.zara.pricing.controller;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
--- src/main/java/com/zara/pricing/service/impl/PriceServiceImpl.java
+++ src/main/java/com/zara/pricing/service/impl/PriceServiceImpl.java
@@ -2,7 +2,6 @@ package com.zara.pricing.service.impl;
 
 import java.time.LocalDateTime;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
